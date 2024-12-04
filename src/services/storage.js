7841929fadc9aeed323356b016b0436cb118d240
GuiSatierf <guideveloper11@gmail.com>
2024-12-04 02:13:10 -0300

    phantom-clone

--- src/services/storage.js
+++
@@ -1,8 +0,0 @@
-export const salvarAnotacoesJSON = (anotacoes) => {
-    localStorage.setItem("anotacoes", JSON.stringify(anotacoes));
-  };
-  
-  export const carregarAnotacoesJSON = () => {
-    return JSON.parse(localStorage.getItem("anotacoes")) || [];
-  };
-  
